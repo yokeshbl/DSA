03fad0bc2c2bc0251fa1466646260df309e0a8e9
yokeshbl <yokeshyokesh606@gmail.com>
2025-10-17 17:45:30 +0530

    Added Code for Reversing LinkedList

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,16 @@ public class Main {
 
     // Method to add element to linked list
     public void add(int data) {
-        Node newNode=new Node(data);
-        if(head==null){
-            head=newNode;
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
             return;
         }
-        Node current=head;
-        while(current.next!=null){
-            current=current.next;
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
         }
-        current.next=newNode;
+        current.next = newNode;
     }
 
     // Method to display linked list elements
@@ -36,13 +36,33 @@ public class Main {
         System.out.println();
     }
 
+    // Method to reverse linked list
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;       // Save next node
+            current.next = prev;       // Reverse current node's pointer
+            prev = current;            // Move prev ahead
+            current = next;            // Move current ahead
+        }
+        head = prev;                   // Update head to new front
+    }
+
     public static void main(String[] args) {
         Main list = new Main();
         list.add(10);
         list.add(20);
         list.add(35);
         list.add(55);
+
         System.out.print("Linked List: ");
+        list.display();
+
+        list.reverse();
+
+        System.out.print("Reversed List: ");
         list.display();
     }
 }
